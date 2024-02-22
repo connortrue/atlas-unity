978ac20864b1103e18bd76ba089ae10492948ae4
connortrue <connor.true>
2024-02-21 22:06:50 -0600

    timer

---
+++ unity-assets_models_textures/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTransform; // Reference to the player's Transform
+    public float rotationSpeed =  100f; // Speed at which the camera rotates
+}
