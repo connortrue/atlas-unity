5f48073d71378dbc3708fd96061bbe589bc7aae7
Connor True <connor.true.94@gmail.com>
2024-04-17 23:49:12 -0500

    real EOD

--- unity-audio/Assets/Scripts/BackgroundMusicController.cs
+++ unity-audio/Assets/Scripts/BackgroundMusicController.cs
@@ -12,10 +12,11 @@ public class BackgroundMusicController : MonoBehaviour
     void Update()
     {
         // Stop the music if the player touches the WinFlag
-        if (/* TD: pull the condition to check if the player has touched the WinFlag */)
+        /*if (/* TD: pull the condition to check if the player has touched the WinFlag )
         {
             audioSource.Stop();
-        }
+        }*/
+        
     }
 }
 
